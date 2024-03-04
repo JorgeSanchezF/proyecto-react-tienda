8ee3fe6a1d5a36e3edfaf24dbbccf70aaf9e7e8f
JorgeSanchezF <miloyjorge@gmail.com>
2024-03-04 08:32:15 +0100

    1

---
+++ src/components/index.jsx
@@ -0,0 +1,38 @@
+function index() {
+    let juegos = [{
+        nombre: 'Rule the Waves 3',
+        precio: 39.99,
+        img: 'rulethewaves'
+    }, {
+        nombre: 'Helldivers 2',
+        precio: 39.99,
+        img: 'helldivers2'
+    },
+    {
+        nombre: 'Pacific Drive',
+        precio: 19.99,
+        img: 'pacificdrive'
+    }, {
+        nombre: 'Rule the Waves 3',
+        precio: 39.99,
+        img: 'rulethewaves'
+    }
+    ];
+
+    array.forEach(element => {
+
+    });
+    return (
+        <div className="index">
+            <head>
+                <title>Bienvendio a La tienda</title>
+            </head>
+
+            <body>
+                <div className="contenedor-cards">
+
+                </div>
+            </body>
+        </div>
+    );
+}
